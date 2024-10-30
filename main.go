9048f4201f600f90851ff617d21ee66d73c255d8
Luis Pereira <luispereira.tkd@gmail.com>
2024-10-30 22:15:10 +0000

    app: sample app to use taskwarrior lib

--- main.go
+++ main.go
@@ -1,46 +1,39 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
-	"os/exec"
-	"sort"
 
-	"github.com/easilok/uiwarrior/models"
+	"github.com/easilok/uiwarrior/lib/taskwarrior"
 )
 
 func main() {
 
-	// exec.Command("task", "context", "none")
-	cmd := exec.Command("task", "export")
-	stdout, err := cmd.Output()
-
-	if err != nil {
+	var taskWarrior taskwarrior.Taskwarrior
+	if err := taskWarrior.Load(); err != nil {
 		fmt.Println(err.Error())
 		return
 	}
 
-	var tasks []models.Task
-	err = json.Unmarshal(stdout, &tasks)
-
-	if err != nil {
-		fmt.Println(err.Error())
-		return
-	}
-
-	pendingTasks := []models.Task{}
-
-	for _, task := range tasks {
-		if task.Status != "completed" {
-			pendingTasks = append(pendingTasks, task)
-		}
-	}
-
-	sort.Slice(pendingTasks, func(i, j int) bool {
-		return pendingTasks[i].Urgency > pendingTasks[j].Urgency
-	})
-
-	for _, task := range pendingTasks {
+	fmt.Println("Completed Tasks")
+	fmt.Println("----------------")
+	for _, task := range taskWarrior.Completed() {
 		fmt.Println(task.Text())
 	}
+
+	fmt.Println()
+	fmt.Println("Pending Tasks")
+	fmt.Println("----------------")
+	for _, task := range taskWarrior.Pending() {
+		fmt.Println(task.Text())
+	}
+
+	fmt.Println()
+	fmt.Println("Projects")
+	fmt.Println("----------------")
+	fmt.Printf("%v\n", taskWarrior.Projects())
+
+	fmt.Println()
+	fmt.Println("Tags")
+	fmt.Println("----------------")
+	fmt.Printf("%v\n", taskWarrior.Tags())
 }
